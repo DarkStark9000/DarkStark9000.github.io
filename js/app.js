51fd76fbc94b378cf8d8cf59c588e55a8eb70a88
Debarshi Das <debarshi.das2019@vitstudent.ac.in>
2022-03-02 22:03:57 +0530

    .

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const  countdown = () => {
+
+    const countDate = new Date("March 14, 2022 00:10:00 GMT+0530").getTime();
+    const now = new Date().getTime();
+    const timeLeft = countDate - now;
+    
+    const sec = 1000;
+    const min = sec * 60;
+    const hour = min * 60;
+    const day = hour * 24
+    
+    const Dayleft = Math.floor(timeLeft / day);
+    const Hourleft = Math.floor((timeLeft % day) / hour);
+    const Minleft = Math.floor((timeLeft % hour) / min);
+    const Secleft = Math.floor((timeLeft % min) / sec);
+    
+    document.querySelector(".days").innerText = Dayleft;
+    document.querySelector(".hours").innerText = Hourleft;
+    document.querySelector(".minutes").innerText = Minleft;
+    document.querySelector(".seconds").innerText = Secleft;
+    
+    }
+    
+    setInterval(countdown, 1000);
+    
